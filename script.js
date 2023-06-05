6081baf07397edbc2c04950c30e8b9d8293b2433
Pavani0707 <93459294+Pavani0707@users.noreply.github.com>
2023-06-05 21:40:59 +0530

    performed first DOM manipulation

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //alert("Hi,Alert Message");
-console.log("Hi,Alert Message");
+//console.log("Hi,Alert Message");
+
+var data1 = {image: "old_picture.jpeg", name :"Pavani", designation: "Intern"};
+var data2 = {image: "new_picture.jpeg", name :"Nakka Pavani", designation: "Software Engineer"};
+var isData1= true;
+var displayData ;
+
+var changeMyPic = function(){
+    if(isData1){
+        displayData= data2;
+        isData1= false;
+    }
+    else{
+        displayData= data1;
+        isData1 = true;
+    }
+    document.getElementById("myimage").src= displayData.image;
+    //document.getElementById("myname").value = displayData.name;
+    //document.getElementById("mydesignattion").value= displayData.designation;
+}
